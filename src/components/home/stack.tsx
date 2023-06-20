1605ad21708e3202e83dbd4f415b64a4f38c5343
LEE-YO-HAN <yhl0078@gmail.com>
2023-06-20 21:39:57 +0900

    TODO: need bubble chart position fix

--- src/components/home/stack.tsx
+++ src/components/home/stack.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React from "react";
 import { InnerContainer, ContentBox } from "../common/styles";
 import { useComponentViewCheck } from "../../hooks/useComponentViewCheck";
 import { publicImage } from "../../util/publicimage";
@@ -22,9 +23,60 @@ const stackArr = [
   mkStack("vercel", "70"),
   mkStack("css", "70"),
   mkStack("git", "50"),
-  mkStack("nestjs", "40"),
-  mkStack("postgreSQL", "20"),
-  mkStack("jest", "10"),
+  mkStack("nestjs", "70"),
+  mkStack("postgreSQL", "50"),
+  mkStack("jest", "30"),
+];
+
+const rc = Math.ceil(stackArr.length);
+const testBubblePosition = new Array(rc).fill(new Array(rc).fill(null));
+
+const center = Math.floor(rc / 2);
+
+stackArr.forEach((v) => {});
+
+console.log(testBubblePosition);
+
+const bubblePositions = [
+  [null, null, null, null, null, null, null],
+  [
+    null,
+    mkStack("nestjs", "70"),
+    mkStack("postgreSQL", "50"),
+    mkStack("jest", "30"),
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    mkStack("git", "50"),
+    mkStack("javascript", "100"),
+    mkStack("typescript", "95"),
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    mkStack("css", "70"),
+    mkStack("react", "100"),
+    mkStack("nextjs", "100"),
+    mkStack("styledcomponents", "90"),
+    null,
+    null,
+  ],
+  [
+    null,
+    mkStack("vercel", "70"),
+    mkStack("reduxToolkit", "80"),
+    mkStack("html", "80"),
+    null,
+    null,
+    null,
+  ],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
 ];
 
 export const Stack = () => {
@@ -40,7 +92,7 @@ export const Stack = () => {
     >
       <h2 id="stackBox">Stack</h2>
       <ContentBox>
-        {stackArr.map((v, i) => {
+        {/* {stackArr.map((v, i) => {
           return (
             <BubbleElement key={i}>
               <img
@@ -51,11 +103,47 @@ export const Stack = () => {
               />
             </BubbleElement>
           );
-        })}
+        })} */}
+        <BubbleChart>
+          {bubblePositions.map((row, rowIndex) => {
+            return (
+              <div className="row" key={rowIndex}>
+                {row.map((col, colIndex) => {
+                  const position = [rowIndex, colIndex];
+                  const dataIndex = bubblePositions[position[0]][position[1]];
+                  if (dataIndex !== null) {
+                    return (
+                      <BubbleElement key={colIndex}>
+                        <img
+                          src={dataIndex.imageSrc}
+                          alt={dataIndex.stack}
+                          width={`${+dataIndex.importance * 2}px`}
+                          height={`${+dataIndex.importance * 2}px`}
+                        />
+                      </BubbleElement>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+            );
+          })}
+        </BubbleChart>
       </ContentBox>
     </InnerContainer>
   );
 };
+
+const BubbleChart = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
+
+  & .row {
+    display: flex;
+    justify-content: center;
+  }
+`;
 
 const BubbleElement = styled.span`
   & img {
